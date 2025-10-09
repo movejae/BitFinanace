d56bb3e48531e340afe1fcf5b38f8e79a90c0056
djlee <dewawd@gmail.com>
2025-10-09 20:46:31 +0900

    feat(dto): 컨트롤러 리턴형식을 responseDTO로 변경

--- src/main/java/com/finpuff/bitfinanace/controller/BitcoinPriceController.java
+++ src/main/java/com/finpuff/bitfinanace/controller/BitcoinPriceController.java
@@ -1,6 +1,6 @@
 package com.finpuff.bitfinanace.controller;
 
-import com.finpuff.bitfinanace.domain.BitcoinPrice;
+import com.finpuff.bitfinanace.dto.BitcoinPriceResponse;
 import com.finpuff.bitfinanace.service.BitcoinPriceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,25 +26,37 @@ public class BitcoinPriceController {
 
     @Operation(summary = "최근 24시간 비트코인 시세 조회", description = "최근 24시간 동안의 모든 비트코인 시세 데이터를 조회합니다.")
     @GetMapping("/day")
-    public ResponseEntity<List<BitcoinPrice>> getDayPrices() {
-        return ResponseEntity.ok(bitcoinPriceService.getDayPrices());
+    public ResponseEntity<List<BitcoinPriceResponse>> getDayPrices() {
+        return ResponseEntity.ok(
+                bitcoinPriceService.getDayPrices().stream()
+                        .map(BitcoinPriceResponse::from)
+		                .toList()
+        );
     }
 
     @Operation(summary = "최근 30일 비트코인 시세 조회", description = "최근 30일 동안의 모든 비트코인 시세 데이터를 조회합니다.")
     @GetMapping("/month")
-    public ResponseEntity<List<BitcoinPrice>> getMonthPrices() {
-        return ResponseEntity.ok(bitcoinPriceService.getMonthPrices());
+    public ResponseEntity<List<BitcoinPriceResponse>> getMonthPrices() {
+        return ResponseEntity.ok(
+                bitcoinPriceService.getMonthPrices().stream()
+                        .map(BitcoinPriceResponse::from)
+		                .toList()
+        );
     }
 
     @Operation(summary = "최근 7일 비트코인 시세 조회", description = "최근 7일 동안의 모든 비트코인 시세 데이터를 조회합니다.")
     @GetMapping("/week")
-    public ResponseEntity<List<BitcoinPrice>> getWeekPrices() {
-        return ResponseEntity.ok(bitcoinPriceService.getWeekPrices());
+    public ResponseEntity<List<BitcoinPriceResponse>> getWeekPrices() {
+        return ResponseEntity.ok(
+                bitcoinPriceService.getWeekPrices().stream()
+                        .map(BitcoinPriceResponse::from)
+		                .toList()
+        );
     }
 
     @Operation(summary = "최신 비트코인 시세 조회", description = "가장 최근의 비트코인 시세 데이터 1건을 조회합니다.")
     @GetMapping("/latest")
-    public ResponseEntity<BitcoinPrice> getLatestPrice() {
-        return ResponseEntity.ok(bitcoinPriceService.getLatestPrice());
+    public ResponseEntity<BitcoinPriceResponse> getLatestPrice() {
+        return ResponseEntity.ok(BitcoinPriceResponse.from(bitcoinPriceService.getLatestPrice()));
     }
 }
